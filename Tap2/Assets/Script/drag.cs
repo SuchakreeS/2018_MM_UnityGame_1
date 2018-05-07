5869dec9860777d7d22f48f0ec0d0d09c0de66fb
memoriE <maxsomonkey@gmail.com>
2018-05-08 02:31:30 +0700

    add object movement

--- Tap2/Assets/Script/drag.cs
+++ Tap2/Assets/Script/drag.cs
@@ -7,10 +7,13 @@ public class drag : MonoBehaviour {
 	public float jumpPower;
 	public Rigidbody rb;
 	private bool isGrounded;
+	private bool right_direction;
+	public GameObject pingu;
 	// Use this for initialization
 	void Start () {
 		moveSpeed = 3f;
 		jumpPower = 8f;
+		right_direction = true;
 		rb = GetComponent<Rigidbody>();
 	}
 	
@@ -20,6 +23,15 @@ public class drag : MonoBehaviour {
 			rb.AddForce(transform.up * jumpPower,ForceMode.Impulse);
 			isGrounded = false;
 		}
+		if (Input.GetKey("left"))
+			pingu.transform.Rotate(0, -20, 0);
+            print("up arrow key is held left");
+        
+        if (Input.GetKey("right"))
+			
+			pingu.transform.Rotate(0, 20, 0);
+            print("down arrow key is held right");
+		
 		transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime, 0f, 0f);
 	}
 

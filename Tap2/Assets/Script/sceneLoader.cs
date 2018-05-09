17e3ced6b69871bfcce98c9539063db6300a1de6
meawkeang <meawkeang@gmail.com>
2018-05-10 04:50:24 +0700

    menu, credits, tutorial

---
+++ Tap2/Assets/Script/sceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneLoader : MonoBehaviour {
+
+    public string sceneName;    // what scene you gonna load
+                                // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void loadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
